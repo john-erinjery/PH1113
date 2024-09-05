531857579a759702cd41bb9f578ec6cc281fd752
john-erinjery <johnerinjery12@gmail.com>
2024-09-05 09:36:15 +0530

    KundtsTube

---
+++ SoundSpeed/main.py
@@ -0,0 +1,45 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+reader = list(csv.reader(open('data.csv', 'r'), quoting=csv.QUOTE_NONNUMERIC))
+figure, ax = plt.subplots(1, 1, figsize=(8, 6), layout='constrained')
+
+def wavelen_calc(data:list):
+    '''
+    data should be distances of nodes and antinodes
+    [n1, a1, n2, a2] n = node; a = anti-node
+
+    we will take all possible values for wavelenght, avergae them out
+    and find wavelength, along with st.dev
+    '''
+    f, n1, a1, n2, a2 = data
+    lambda_values = [(a1-n1)*4, (n2 - a1)*4, (a2 - n2)*4, (n2-n1)*2, (a2 - a1)*2]
+    mean = np.mean(lambda_values)
+    std = round(np.std(lambda_values), 3)
+    
+    return f, mean, std
+
+
+
+y_values = []
+x_values_ = []
+err=[]
+
+for i in reader:
+
+    values = wavelen_calc(i)
+    y_values.append(values[0])
+    x_values_.append(values[1])
+    err.append(values[2])
+y_values = np.array(y_values)
+ar = np.array(x_values_)
+x_values = 1/ar
+a, b = np.polyfit(x_values, y_values, 1)
+plt.scatter(x_values, y_values)
+plt.errorbar(x_values, y_values, [float(x) for x in x_values], ecolor="red")
+x = np.linspace(0 ,0.01, 20)
+print(err)
+plt.plot(x, a*x + b, '--', color="skyblue", label=f"slope : {round(a/1000, 3)}")
+plt.legend()
+plt.show()
+    
